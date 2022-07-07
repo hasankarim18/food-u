cf981e948a13b324f9f72c81b6cfdfd3bb81931e
hasankarim18 <66988931+hasankarim18@users.noreply.github.com>
2022-07-07 21:37:27 +0600

    Adding Router /1. Preparation for adding router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import MainComponent from './components/MainComponent';
 import './App.css';
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom'
 
 function App() {
   return (
-    <React.Fragment>
+    <BrowserRouter>
       <MainComponent />
-    </React.Fragment>
+    </BrowserRouter>
+
   );
 }
 
--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import Menu from './Menu'
+import Home from './Home'
+import About from './About'
+import Contact from './Contact'
 
 const Body = () => {
     return (
         <div className="container">
-            <Menu />
+
         </div>
     )
 }
