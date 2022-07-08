7fd0acb4a516af8246cc50f04e3987d5d395add2
hasankarim18 <66988931+hasankarim18@users.noreply.github.com>
2022-07-08 22:58:20 +0600

    Applying Redux 1. Reducer for React app

---
+++ src/redux/reducer.js
@@ -0,0 +1,14 @@
+import DISHES from '../data/dishes'
+import COMMENTS from '../data/comments'
+
+const initialStae = {
+    dishes: DISHES,
+    comments: COMMENTS
+}
+
+const Reducer = (state = initialStae, action) => {
+    return state
+}
+
+
+export { Reducer }
