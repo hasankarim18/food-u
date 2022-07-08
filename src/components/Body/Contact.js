c3e66d1361552f1e378e8835648014de25c9627a
hasankarim18 <66988931+hasankarim18@users.noreply.github.com>
2022-07-08 09:49:51 +0600

    3 Contact form handling

--- src/components/Body/Contact.js
+++ src/components/Body/Contact.js
@@ -14,16 +14,33 @@ export class Contact extends Component {
             contactType: 'Tel',
             message: ''
         }
+
+        this.inputChangeHandleer = this.inputChangeHandleer.bind(this)
+        this.onSubmitHandler = this.onSubmitHandler.bind(this)
+    }
+
+    inputChangeHandleer = event => {
+        const value = event.target.type === 'checkbox' ? event.target.checked : event.target.value
+
+        const name = event.target.name
+        this.setState({
+            [name]: value
+        })
+    }
+
+    onSubmitHandler = event => {
+        event.preventDefault()
+        console.log(this.state)
     }
 
     render() {
         return (
-            <div className="row row-content" >
+            <div className="row row-content" style={{ textAlign: "left" }} >
                 <div className="col-12">
                     <h3>Send us your feedback</h3>
                 </div>
                 <div className="col-12">
-                    <Form>
+                    <Form onSubmit={this.onSubmitHandler} >
                         <FormGroup row>
                             <Label htmlFor='firstName' md={2}> FirstName </Label>
                             <Col md={10}>
@@ -32,6 +49,7 @@ export class Contact extends Component {
                                     name="firstName"
                                     placeholder='First Name'
                                     value={this.state.firstName}
+                                    onChange={this.inputChangeHandleer}
                                 />
                             </Col>
                         </FormGroup>
@@ -43,6 +61,7 @@ export class Contact extends Component {
                                     name="lastName"
                                     placeholder='Last Name'
                                     value={this.state.lastName}
+                                    onChange={this.inputChangeHandleer}
                                 />
                             </Col>
                         </FormGroup>
@@ -54,6 +73,7 @@ export class Contact extends Component {
                                     name="telnum"
                                     placeholder='+88 01****'
                                     value={this.state.telnum}
+                                    onChange={this.inputChangeHandleer}
                                 />
                             </Col>
                         </FormGroup>
@@ -65,17 +85,19 @@ export class Contact extends Component {
                                     name="email"
                                     placeholder='jhon@domain.com'
                                     value={this.state.email}
+                                    onChange={this.inputChangeHandleer}
                                 />
                             </Col>
                         </FormGroup>
                         <FormGroup row  >
 
-                            <Col className="d-flex align-items-center" md={{ size: 6, offset: 2 }}>
+                            <Col className="d-flex align-items-center" md={{ size: 4, offset: 2 }}>
                                 <Input
                                     className="m-0"
                                     type='checkbox'
                                     name="agree"
                                     checked={this.state.agree}
+                                    onChange={this.inputChangeHandleer}
                                 />
                                 <Label className="ms-3" htmlFor='agree' md={10}> May we contact you </Label>
                             </Col>
@@ -84,6 +106,7 @@ export class Contact extends Component {
                                     type='select'
                                     name='contactType'
                                     value={this.state.contactType}
+                                    onChange={this.inputChangeHandleer}
                                 >
                                     <option  > Tel. </option>
                                     <option > Email </option>
@@ -98,6 +121,7 @@ export class Contact extends Component {
                                     name="message"
                                     placeholder='Very tasty'
                                     value={this.state.message}
+                                    onChange={this.inputChangeHandleer}
                                 />
                             </Col>
                         </FormGroup>
